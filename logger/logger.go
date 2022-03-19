cc9f8dccdf7268e427051e855fe91192099e46c8
Tom Moulard <tom.moulard@epita.fr>
2022-03-19 15:23:59 +0100

    chore: update logger.go

--- logger/logger.go
+++ logger/logger.go
@@ -6,16 +6,20 @@ import (
 	"path"
 
 	"github.com/cooperspencer/gickup/types"
-
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+// NewRollingFile TODO.
 func NewRollingFile(config types.FileLogging) io.Writer {
 	if config.Dir != "" {
 		if err := os.MkdirAll(config.Dir, 0o744); err != nil {
-			log.Error().Err(err).Str("path", config.Dir).Msg("can't create log directory")
+			log.Error().
+				Err(err).
+				Str("path", config.Dir).
+				Msg("can't create log directory")
+
 			return nil
 		}
 	} else {
@@ -28,6 +32,7 @@ func NewRollingFile(config types.FileLogging) io.Writer {
 	}
 }
 
+// CreateLogger create an instance of Logger.
 func CreateLogger(conf types.Logging) zerolog.Logger {
 	var writers []io.Writer
 
@@ -36,6 +41,7 @@ func CreateLogger(conf types.Logging) zerolog.Logger {
 	if conf.FileLogging.File != "" {
 		writers = append(writers, NewRollingFile(conf.FileLogging))
 	}
+
 	mw := io.MultiWriter(writers...)
 
 	return zerolog.New(mw).With().Timestamp().Logger()
