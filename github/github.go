a8340d7fbc93f38bbb8fbde52adf5a197548b3dd
Tom Moulard <tom.moulard@epita.fr>
2022-03-20 09:44:01 +0100

    chore: update github.go

--- github/github.go
+++ github/github.go
@@ -10,21 +10,43 @@ import (
 )
 
 func addWiki(r github.Repository, repo types.GenRepo, token string) types.Repo {
-	if r.GetHasWiki() && repo.Wiki && types.StatRemote(r.GetCloneURL(), r.GetSSHURL(), repo) {
-		return types.Repo{Name: *r.Name + ".wiki", Url: types.DotGitRx.ReplaceAllString(r.GetCloneURL(), ".wiki.git"), SshUrl: types.DotGitRx.ReplaceAllString(r.GetSSHURL(), ".wiki.git"), Token: token, Defaultbranch: r.GetDefaultBranch(), Origin: repo, Owner: r.GetOwner().GetLogin(), Hoster: "github.com"}
+	if !(r.GetHasWiki() && repo.Wiki &&
+		types.StatRemote(r.GetCloneURL(), r.GetSSHURL(), repo)) {
+		return types.Repo{}
 	}
-	return types.Repo{}
+
+	return types.Repo{
+		Name:          *r.Name + ".wiki",
+		URL:           types.DotGitRx.ReplaceAllString(r.GetCloneURL(), ".wiki.git"),
+		SSHURL:        types.DotGitRx.ReplaceAllString(r.GetSSHURL(), ".wiki.git"),
+		Token:         token,
+		Defaultbranch: r.GetDefaultBranch(),
+		Origin:        repo,
+		Owner:         r.GetOwner().GetLogin(),
+		Hoster:        "github.com",
+	}
 }
 
+// Get TODO.
 func Get(conf *types.Conf) []types.Repo {
 	repos := []types.Repo{}
 	for _, repo := range conf.Source.Github {
-		log.Info().Str("stage", "github").Str("url", "https://github.com").Msgf("grabbing the repositories from %s", repo.User)
-		client := &github.Client{}
-		opt := &github.RepositoryListOptions{ListOptions: github.ListOptions{PerPage: 50}}
+		log.Info().
+			Str("stage", "github").
+			Str("url", "https://github.com").
+			Msgf("grabbing the repositories from %s", repo.User)
+
+		opt := &github.RepositoryListOptions{
+			ListOptions: github.ListOptions{
+				PerPage: 50,
+			},
+		}
+
 		i := 1
 		githubrepos := []*github.Repository{}
 		token := repo.GetToken()
+
+		var client *github.Client
 		if token == "" {
 			client = github.NewClient(nil)
 		} else {
@@ -32,23 +54,34 @@ func Get(conf *types.Conf) []types.Repo {
 				&oauth2.Token{AccessToken: token},
 			)
 			tc := oauth2.NewClient(context.TODO(), ts)
+
 			client = github.NewClient(tc)
 		}
+
 		if token != "" {
 			user, _, err := client.Users.Get(context.TODO(), "")
 			if err != nil {
-				log.Fatal().Str("stage", "github").Str("url", "https://github.com").Msg(err.Error())
+				log.Fatal().
+					Str("stage", "github").
+					Str("url", "https://github.com").
+					Msg(err.Error())
 			}
+
 			if repo.User == user.GetLogin() {
 				repo.User = ""
 			}
 		}
+
 		for {
 			opt.Page = i
 			repos, _, err := client.Repositories.List(context.TODO(), repo.User, opt)
 			if err != nil {
-				log.Fatal().Str("stage", "github").Str("url", "https://github.com").Msg(err.Error())
+				log.Fatal().
+					Str("stage", "github").
+					Str("url", "https://github.com").
+					Msg(err.Error())
 			}
+
 			if len(repos) == 0 {
 				break
 			}
@@ -58,12 +91,20 @@ func Get(conf *types.Conf) []types.Repo {
 
 		if repo.Starred {
 			i = 1
-			opt := &github.ActivityListStarredOptions{ListOptions: github.ListOptions{PerPage: 50}}
+			opt := &github.ActivityListStarredOptions{
+				ListOptions: github.ListOptions{
+					PerPage: 50,
+				},
+			}
+
 			for {
 				opt.ListOptions.Page = i
 				repos, _, err := client.Activity.ListStarred(context.TODO(), repo.User, opt)
 				if err != nil {
-					log.Fatal().Str("stage", "github").Str("url", "https://github.com").Msg(err.Error())
+					log.Fatal().
+						Str("stage", "github").
+						Str("url", "https://github.com").
+						Msg(err.Error())
 				}
 				if len(repos) == 0 {
 					break
@@ -82,30 +123,61 @@ func Get(conf *types.Conf) []types.Repo {
 
 		for _, r := range githubrepos {
 			if include[*r.Name] {
-				repos = append(repos, types.Repo{Name: r.GetName(), Url: r.GetCloneURL(), SshUrl: r.GetSSHURL(), Token: token, Defaultbranch: r.GetDefaultBranch(), Origin: repo, Owner: r.GetOwner().GetLogin(), Hoster: "github.com"})
+				repos = append(repos, types.Repo{
+					Name:          r.GetName(),
+					URL:           r.GetCloneURL(),
+					SSHURL:        r.GetSSHURL(),
+					Token:         token,
+					Defaultbranch: r.GetDefaultBranch(),
+					Origin:        repo,
+					Owner:         r.GetOwner().GetLogin(),
+					Hoster:        "github.com",
+				})
 				wiki := addWiki(*r, repo, token)
 				if wiki.Name != "" {
 					repos = append(repos, wiki)
 				}
+
 				continue
 			}
+
 			if exclude[*r.Name] {
 				continue
 			}
+
 			if excludeorgs[r.GetOwner().GetLogin()] {
 				continue
 			}
+
 			if len(repo.Include) == 0 {
 				if len(includeorgs) > 0 {
 					if includeorgs[r.GetOwner().GetLogin()] {
-						repos = append(repos, types.Repo{Name: r.GetName(), Url: r.GetCloneURL(), SshUrl: r.GetSSHURL(), Token: token, Defaultbranch: r.GetDefaultBranch(), Origin: repo, Owner: r.GetOwner().GetLogin(), Hoster: "github.com"})
+						repos = append(repos, types.Repo{
+							Name:          r.GetName(),
+							URL:           r.GetCloneURL(),
+							SSHURL:        r.GetSSHURL(),
+							Token:         token,
+							Defaultbranch: r.GetDefaultBranch(),
+							Origin:        repo,
+							Owner:         r.GetOwner().GetLogin(),
+							Hoster:        "github.com",
+						})
 						wiki := addWiki(*r, repo, token)
 						if wiki.Name != "" {
 							repos = append(repos, wiki)
 						}
 					}
 				} else {
-					repos = append(repos, types.Repo{Name: r.GetName(), Url: r.GetCloneURL(), SshUrl: r.GetSSHURL(), Token: token, Defaultbranch: r.GetDefaultBranch(), Origin: repo, Owner: r.GetOwner().GetLogin(), Hoster: "github.com"})
+					repos = append(repos, types.Repo{
+						Name:          r.GetName(),
+						URL:           r.GetCloneURL(),
+						SSHURL:        r.GetSSHURL(),
+						Token:         token,
+						Defaultbranch: r.GetDefaultBranch(),
+						Origin:        repo,
+						Owner:         r.GetOwner().GetLogin(),
+						Hoster:        "github.com",
+					})
 					wiki := addWiki(*r, repo, token)
 					if wiki.Name != "" {
 						repos = append(repos, wiki)
@@ -114,5 +186,6 @@ func Get(conf *types.Conf) []types.Repo {
 			}
 		}
 	}
+
 	return repos
 }
