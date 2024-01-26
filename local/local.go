4768f160678d457248a43a39c43428c348f16eba
Andreas Wachter <andreas.wachter@buddyspencer.monster>
2024-01-26 08:58:59 +0100

    do not fetch

--- local/local.go
+++ local/local.go
@@ -467,16 +467,19 @@ func TempClone(repo types.Repo, tempdir string) (*git.Repository, error) {
 		return nil, err
 	}
 
-	err = r.Fetch(&git.FetchOptions{
-		RefSpecs: []config.RefSpec{"refs/*:refs/*"},
-		Auth:     auth,
-		Force:    true,
-	})
-	if err == git.NoErrAlreadyUpToDate {
-		return r, nil
-	} else {
-		return r, err
-	}
+	return r, err
+	/*
+		err = r.Fetch(&git.FetchOptions{
+			RefSpecs: []config.RefSpec{"refs/*:refs/*"},
+			Auth:     auth,
+			Force:    true,
+		})
+		if err == git.NoErrAlreadyUpToDate {
+			return r, nil
+		} else {
+			return r, err
+		}
+	*/
 }
 
 func CreateRemotePush(repo *git.Repository, destination types.GenRepo, url string) error {
